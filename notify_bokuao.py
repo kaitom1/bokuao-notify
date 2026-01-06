67b835863a7c464d21f10fdc32857f5ae9197036
kaitom1 <kaitotomida@gmail.com>
2026-01-06 12:38:51 +0900

    notify_bokuao.py を更新

--- notify_bokuao.py
+++ notify_bokuao.py
@@ -34,7 +34,7 @@ WEBHOOKS_BY_AUTHOR: Dict[str, str] = {
     "金澤亜美": os.environ["AMI_KANAZAWA"],
     "早﨑すずき": os.environ["SUZUKI_HAYASAKI"],
     #"安納蒼衣": os.environ["AOI_ANNO"],
-    #"塩釜菜那": os.environ["NANA_SHIOGAMA"],
+    "塩釜菜那": os.environ["NANA_SHIOGAMA"],
     #"萩原心花": os.environ["KOKOKA_HAGIWARA"],
     #"工藤唯愛": os.environ["YUA_KUDO"],
     #"須永心海": os.environ["MIUNA_SUNAGA"],
